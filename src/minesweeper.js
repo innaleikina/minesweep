157b81153ce1f834fe1201494a4c784e13af7ecf
Inna <designbyinna@gmail.com>
2017-08-11 19:49:25 -0600

    printBoard issue

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,7 +1,41 @@
-const generatePlayerBoard = (numberoORows, numberOfColumns) => {
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 let board = [];
 
-for(numberOfRowsIndex=0; numberOfRowsIndex < numberOfRows.length; numberOfRowsIndex++){
+for(let numberOfRowsIndex=0; numberOfRowsIndex < numberOfRows; numberOfRowsIndex++){
   let row =[];
-}
+    for (let numberOfColumnsIndex = 0; numberOfColumnsIndex < numberOfColumns; numberOfColumnsIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
+
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+
+  for(let numberOfRowsIndex=0; numberOfRowsIndex < numberOfRows; numberOfRowsIndex++){
+    let row =[];
+      for (let numberOfColumnsIndex = 0; numberOfColumnsIndex < numberOfColumns; numberOfColumnsIndex++) {
+        row.push(null);
+      }
+      board.push(row);
+    }
+    return board;
+
+
+    let numberOfBombsPlaced = 0;
+
+    while(numberOfBombsPlaced < numberOfBombs) {
+    }
+
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+};
+
+const printBoard = console.log(board.map(row => row.join(' | ')).join('/n')
+);
